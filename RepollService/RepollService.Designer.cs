0dbf869ad1de15047726f675b2b849743e687fe5
clarkmeister <clark.arthur57@gmail.com>
2021-06-16 00:50:45 -0700

    added repoll project

---
+++ RepollService/RepollService.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+namespace RepollService
+{
+    partial class RepollService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.repollEventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.repollEventLog)).BeginInit();
+            // 
+            // RepollService
+            // 
+            this.ServiceName = "RepollService";
+            ((System.ComponentModel.ISupportInitialize)(this.repollEventLog)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog repollEventLog;
+    }
+}
