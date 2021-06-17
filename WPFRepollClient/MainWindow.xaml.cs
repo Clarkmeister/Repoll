3dc24747d3829259d203ecbcce0b2c4e0be18493
clarkmeister <clark.arthur57@gmail.com>
2021-06-17 13:11:24 -0700

    Switched client to WPF

---
+++ WPFRepollClient/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RepollInterfaces;
+
+namespace WPFRepollClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+            foreach (ServiceController scTemp in scServices)
+            {
+                if (scTemp.ServiceName == "RepollService")
+                {
+                    ServiceController sc = new ServiceController("RepollService");
+                    if (sc.Status == ServiceControllerStatus.Stopped)
+                    {
+                        sc.Start();
+                        while (sc.Status == ServiceControllerStatus.Stopped)
+                        {
+                            Thread.Sleep(1000);
+                            sc.Refresh();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = "net.tcp://localhost:6565/RepollService";
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            var channel = new ChannelFactory<IWCFRepollService>(binding);
+            var endpoint = new EndpointAddress(uri);
+            var proxy = channel.CreateChannel(endpoint);
+            products = proxy.ListProducts();
+
+            foreach (var product in products)
+            {
+                TestBox.Text += product + "\n";
+            }
+        }
+    }
+}
