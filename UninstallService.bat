f637b147b48b54996b3ede57fb32cc0f51699e53
clarkmeister <clark.arthur57@gmail.com>
2021-06-28 16:21:42 -0700

    Added file removal to uninstall script

--- UninstallService.bat
+++ UninstallService.bat
@@ -1,3 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 installutil -u "%~dp0RepollService\bin\Debug\RepollService.exe"
+echo "Deleting Repoll Files..."
+echo "Deleting: C:\ProgramData\Repoll\repos.json"
+@RD /S /Q "C:\ProgramData\Repoll\"
+echo "Completed - Press any key to exit..."
 PAUSE
